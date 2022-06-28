c9cf97812ae0a6e530649a08c30f95772ed9d553
Rodrigo Blejman <rodrigo.blej@gmail.com>
2022-06-28 20:21:34 -0300

    added function w promise to cycle Qs with delay

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import qSets from "./questions.js";
 const currentQ = document.querySelector(".qWall__question");
 const startBtn = document.querySelector(".start");
 const nextBtn = document.querySelector(".next");
-const defDelay = [1, 4000, 3000, 2000];
-const answered = [];
+const defDelay = [4000, 5000, 6000, 7000];
+const answeredSet = [];
 let next = false;
 
-// select a random Set of questions
+// select a random Set of questions, add it to answeredSet Sets then display.
 function selectRandom(qSets) {
   let qSet = qSets[Math.floor(Math.random() * qSets.length)];
   const { questions, id } = qSet;
@@ -14,22 +14,39 @@ function selectRandom(qSets) {
   showQuestions(questions);
 }
 
-// cycle questions
+// cycle questions with delay
+let promise = Promise.resolve();
 function showQuestions(questions) {
-  questions.forEach((q) => {
-    qIndex = questions.indexOf(q);
-    console.log(q);
-    console.log(defDelay[qIndex]);
-    currentQ.innerHTML = `
-          <h2 class="qWall__question">${q}</h2>`;
-    // countdown pasarle el index de la pregunta
+  questions.forEach(function (item, index) {
+    promise = promise.then(function () {
+      console.log(item);
+      return new Promise(function (resolve) {
+        setTimeout(resolve, defDelay[index]);
+        console.log(`delay of ${defDelay[index]} miliseconds`);
+      });
+    });
   });
 }
 
-// Saving answered question Sets
+promise.then(function () {
+  console.log("Loop finished.");
+});
+
+// function showQuestions(questions) {
+//   questions.forEach((item, index) => {
+//     console.log(item);
+//     console.log(index);
+//     console.log(defDelay[index]);
+//     currentQ.innerHTML = `
+//           <h2 class="qWall__question">${q}</h2>`;
+//     // countdown pasarle el index de la pregunta
+//   });
+// }
+
+// Saving answeredSet question Sets
 function addAnswered(id) {
-  if (!answered.includes(id)) {
-    answered.push(id);
+  if (!answeredSet.includes(id)) {
+    answeredSet.push(id);
   }
 }
 
